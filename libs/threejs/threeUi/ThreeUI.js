8179842f39a4447c19f33e2cd1211dd5b543e004
xiao8xiao1 <xiao8xiao_1@qq.com>
2018-10-08 23:19:24 +0800

    mat

--- libs/threejs/threeUi/ThreeUI.js
+++ libs/threejs/threeUi/ThreeUI.js
@@ -62,9 +62,10 @@ var ThreeUI = function(gameCanvas, height) {
 		this.width* DPR / 2,
 		this.height* DPR / 2,
 		-this.height* DPR / 2,
-		0, 30
+		1, 5
 	);
-	this.camera.position.z = 10;
+	this.camera.position.z = 2;
+	this.camera.lookAt(0,0,0)
 	this.scene = new THREE.Scene();	
 	this.movobjCnt = 0;
 	this.hasMoved = false;
@@ -73,7 +74,8 @@ var ThreeUI = function(gameCanvas, height) {
 	this.prepareThreeJSScene();
 	// Event listening
     // canvas.addEventListener('touchstart', this.clickHandler.bind(this));
-    canvas.addEventListener('touchend', this.clickHandler.bind(this));
+	canvas.addEventListener('touchend', this.clickHandler.bind(this));
+	console.log('uiCam', this.camera.position)
 };
 
 /**
@@ -105,10 +107,11 @@ ThreeUI.prototype.prepareThreeJSScene = function() {
 	console.log('plane', this.width* DPR, this.height* DPR)
 	this.plane = new THREE.Mesh(planeGeo, material);
 	this.plane.matrixAutoUpdate = false;
-	// this.plane.translateZ(-1)
+	this.plane.translateZ(1)
 	// this.plane.updateMatrix()
 	this.scene.add(this.plane);
 	this.texture.needsUpdate = true;
+	console.log('uiPlane', this.plane.position)
 };
 
 /**
@@ -448,6 +451,7 @@ ThreeUI.prototype.setPlaneMovHeight = function(newHeight) {
 	this.planeMov.updateMatrix()
 	this.scene.add(this.planeMov);
 	this.textureMov.needsUpdate = true;
+	console.log('uiPlaneMov', this.planeMov.position)
 }
 
 ThreeUI.prototype.enableMov = function() {
